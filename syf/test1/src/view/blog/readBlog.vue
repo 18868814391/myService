66597dc9b740a32ccc4f4f8195cab0069c5f9e45
pptvvx <605729908@qq.com>
2019-06-06 11:03:39 +0800

    123

--- syf/test1/src/view/blog/readBlog.vue
+++ syf/test1/src/view/blog/readBlog.vue
@@ -11,26 +11,57 @@
 <script>
 import { blogDetail } from '@/api';
 import { quillEditor } from 'vue-quill-editor'
+import {Toast} from 'vant';
 export default {
+  components: {
+    [Toast.name]: Toast,
+  },   
   data(){
     return{
       id:'',
       con:'',
       richText:'',
+      toast:'',
+      timer:'',
     }
   },
   created(){
-    const self=this;
+     const self=this;   
+      let toast = Toast.loading({
+        duration: 0,       // 持续展示 toast
+        forbidClick: true, // 禁用背景点击
+        loadingType: 'spinner',
+        message: 'loading... 3 秒'
+      });
+      let second = 3;
+
+      let timer = setInterval(() => {
+        second--;
+        if (second>=0) {
+          self.toast.message = `loading... ${second} 秒`;
+        } else if(second<0&&second>=-3){
+          self.toast.message = `好吧，我的服务器是捡来的`;
+        }else if(second<-3&&second>-6){
+          self.toast.message = `或许是你的网络问题，我关掉了嗷`;
+        }else if(second<=-6){
+          Toast.clear();
+          clearInterval(self.timer);          
+        }
+      }, 1000);      
+      
     this.id=this.$route.query.id;
     blogDetail({
       'id':self.id
     }).then((d)=>{
+      Toast.clear();
+      clearInterval(self.timer);  
       self.con=d.data.data;
       self.richText=self.con.content;
       console.log(self.richText);
       self.goTop();
     }).catch(()=>{
-
+      Toast.clear();
+      clearInterval(self.timer);  
     })
   },
   methods:{
