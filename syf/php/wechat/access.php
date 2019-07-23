3ba8a56d63248c113163113057f3b174c25532de
pptvvx <605729908@qq.com>
2019-07-23 10:21:30 +0800

    12345

--- syf/php/wechat/access.php
+++ syf/php/wechat/access.php
@@ -22,7 +22,24 @@ header('content-type:text/html;charset=utf-8');
     echo "\r\n";
     var_dump($resArr['access_token']);
     echo "\r\n";
+    $accToken=$resArr['access_token'];  //获取到了accessToken
     curl_close($ch);
 
-
+    $url2="https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$accToken."&type=jsapi"
+    $ch2 = curl_init($url);
+    curl_setopt($ch2,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);//跳过证书验证
+    curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, false);  // 从证书中检查SSL加密算法是否存在
+    $res2 = curl_exec($ch2);
+    $res2 = curl_exec($ch2);
+    if(curl_errno($ch2)){
+        var_dump(curl_error($ch2));
+    }
+    $resArr2 = json_decode($res2,1);
+    echo "签名算法";
+    echo "\r\n";
+    echo urldecode(json_encode($resArr2));
+    echo "\r\n";
+    echo $resArr2['ticket'];
+    curl_close($ch2);
 ?>
